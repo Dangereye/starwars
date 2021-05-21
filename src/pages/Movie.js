afc0954c7a4a6d527ac4921812cee7fde3b5a22f
Craig Puxty <craig_puxty@talktalk.net>
2021-05-21 22:02:11 +0100

    Removed console log

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -14,7 +14,6 @@ const Movie = () => {
     error,
     data: movie,
   } = useFetch(`https://swapi.dev/api/films/${id}`);
-  console.log(movie);
   useEffect(() => {
     window.scrollTo(0, 0);
   });
